ad0831d770030e16ba5f8624d3305c7e0e772b28
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2019-12-18 14:06:31 -0500

    added JVM memory options to _JAVA_OPTIONS

--- maven-host-cache.sh
+++ maven-host-cache.sh
@@ -3,3 +3,4 @@
 docker run -v ~/.m2:/root/.m2 -v "$PWD":/usr/src -w /usr/src maven:3-jdk-8 mvn clean package
 docker rm -f spring-boot-rest-example
 docker build  -f Dockerfile.maven-host-cache -t spring-boot-rest-example .
+docker run -m500M --name spring-boot-rest-example -p 8080:8080 -p 8181:8081 -p 8778:8778 spring-boot-rest-example:latest
--- maven-multi-stage-layer-cached.sh
+++ maven-multi-stage-layer-cached.sh
@@ -2,3 +2,4 @@
 
 docker rm -f spring-boot-rest-example
 docker build  -f Dockerfile.maven-multi-stage-layer-cached -t spring-boot-rest-example .
+docker run -m500M --name spring-boot-rest-example -p 8080:8080 -p 8181:8081 -p 8778:8778 spring-boot-rest-example:latest
