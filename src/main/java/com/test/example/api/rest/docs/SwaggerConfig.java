17259177638829a07c359851a42ff5d866f0c65a
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2022-01-26 15:31:10 -0500

    updated pom.xml

--- src/main/java/com/test/example/api/rest/docs/SwaggerConfig.java
+++ src/main/java/com/test/example/api/rest/docs/SwaggerConfig.java
@@ -23,7 +23,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(Predicates.not(PathSelectors.regex("/error")))
+                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
                 .build()
                 .apiInfo(apiInfo());
     }
